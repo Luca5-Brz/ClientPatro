eeda2ad41cb3dfe951dbf8f73306cd9264dafa25
LucasBARREZ <lucas.barrez@std.heh.be>
2024-07-01 21:21:11 +0200

    Test

--- header.php
+++ header.php
@@ -2,4 +2,8 @@
      <img src="img/logo.png" alt="Logo Patro" id="logo">
      <h1>Patro d'Ollignies</h1>
      <p>Nom Prenom</p>
+     <nav id="navBar">
+          <button>Gestion des cartes</button>
+          <button>Menu</button>
+     </nav>
 </div>
