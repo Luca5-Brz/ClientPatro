dedce10f3dcb98026337e592b5548cb4eb304b49
Delcroix Matthias <matthiasdelcroix@hotmail.com>
2024-07-01 22:30:23 +0200

    Test local

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
      <h1 id="titre">Patro d'Ollignies</h1>
      <p id="infos">Nom Prenom</p>
      <nav id="navBar">
-          <button>Info</button>
+          <button>Info g'tre</button>
           <button>Menu</button>
      </nav>
 </div>
